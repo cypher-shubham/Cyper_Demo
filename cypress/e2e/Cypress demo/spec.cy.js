eeef262b6dd6c4ed645c173ac5f21845a618de6f
Shubham Singh <sh20104733@tx15qp4m9g.home>
2023-05-31 12:27:05 +0100

    first commit

--- cypress/e2e/Cypress demo/spec.cy.js
+++ cypress/e2e/Cypress demo/spec.cy.js
@@ -1,8 +1,8 @@
 describe('My First Test', () => {
   it('Visit Guru 99 ', () => {
     cy.visit('https://www.guru99.com/')
-    //cy.get('[data-cy="➤ Software Testing"]').click()
     cy.contains("➤ Software Testing").click()
     cy.contains('About Us').click()
+    //cy.get('[data-cy="➤ Software Testing"]').click()
   })
 })
